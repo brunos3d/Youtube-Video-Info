88e746248cab80838ddb1c95ff188591779f0cc3
Bruno Silva <bruno3dcontato@gmail.com>
2019-09-21 16:39:30 -0300

    stylized table

--- pages/video/[id]/index.js
+++ pages/video/[id]/index.js
@@ -41,75 +41,79 @@ const VideoInfo = (props) => {
 
 	return (
 		<div className="main-container">
-			<table>
-				<tr>
-					<th>Propriedades</th>
-					<th>Valores</th>
-				</tr>
-				<tr>
-					<td>Título</td>
-					<td>{title}</td>
-				</tr>
-				<tr>
-					<td>Canal</td>
-					<td>{owner}</td>
-				</tr>
-				<tr>
-					<td>Views</td>
-					<td>{formatNumber(views)}</td>
-				</tr>
-				<tr>
-					<td>Likes</td>
-					<td>{formatNumber(likeCount)}</td>
-				</tr>
-				<tr>
-					<td>Dislikes</td>
-					<td>{formatNumber(dislikeCount)}</td>
-				</tr>
-				<tr>
-					<td>Comentários</td>
-					<td>{commentCount}</td>
-				</tr>
-				<tr>
-					<td>Duração</td>
-					<td>{videoDurationFormated} (HH:MM:SS)</td>
-				</tr>
-				<tr>
-					<td>Data de publicação</td>
-					<td>{timestamp.format("L")} ({timestamp.fromNow()})</td>
-				</tr>
-				<tr>
-					<td>ID do Video</td>
-					<td>{videoId}</td>
-				</tr>
-				<tr>
-					<td>URL</td>
-					<td>{url}</td>
-				</tr>
-				<tr>
-					<td>ID do Canal</td>
-					<td>{channelId}</td>
-				</tr>
-				<tr>
-					<td>Gênero</td>
-					<td>{genre.toString()}</td>
-				</tr>
-				<tr>
-					<td>Não listado</td>
-					<td>{formatBoolean(unlisted)}</td>
-				</tr>
-				<tr>
-					<td>Contém promoção paga</td>
-					<td>{formatBoolean(paid)}</td>
-				</tr>
-				<tr>
-					<td>Livre para todos (Family-Friendly)</td>
-					<td>{formatBoolean(isFamilyFriendly)}</td>
-				</tr>
-				<tr>
-					<td>Thumbnail URL</td>
-					<td>{channelThumbnailUrl}</td>
-				</tr>
+			<table className="table-rounded">
+				<thead className="table-header">
+					<tr>
+						<th>Propriedades</th>
+						<th>Valores</th>
+					</tr>
+				</thead>
+				<tbody className="table-body">
+					<tr>
+						<td>Título</td>
+						<td>{title}</td>
+					</tr>
+					<tr>
+						<td>Canal</td>
+						<td>{owner}</td>
+					</tr>
+					<tr>
+						<td>Views</td>
+						<td>{formatNumber(views)}</td>
+					</tr>
+					<tr>
+						<td>Likes</td>
+						<td>{formatNumber(likeCount)}</td>
+					</tr>
+					<tr>
+						<td>Dislikes</td>
+						<td>{formatNumber(dislikeCount)}</td>
+					</tr>
+					<tr>
+						<td>Comentários</td>
+						<td>{commentCount}</td>
+					</tr>
+					<tr>
+						<td>Duração</td>
+						<td>{videoDurationFormated} (HH:MM:SS)</td>
+					</tr>
+					<tr>
+						<td>Data de publicação</td>
+						<td>{timestamp.format("L")} ({timestamp.fromNow()})</td>
+					</tr>
+					<tr>
+						<td>ID do Video</td>
+						<td>{videoId}</td>
+					</tr>
+					<tr>
+						<td>URL</td>
+						<td>{url}</td>
+					</tr>
+					<tr>
+						<td>ID do Canal</td>
+						<td>{channelId}</td>
+					</tr>
+					<tr>
+						<td>Gênero</td>
+						<td>{genre.toString()}</td>
+					</tr>
+					<tr>
+						<td>Não listado</td>
+						<td>{formatBoolean(unlisted)}</td>
+					</tr>
+					<tr>
+						<td>Contém promoção paga</td>
+						<td>{formatBoolean(paid)}</td>
+					</tr>
+					<tr>
+						<td>Livre para todos (Family-Friendly)</td>
+						<td>{formatBoolean(isFamilyFriendly)}</td>
+					</tr>
+					<tr>
+						<td>Thumbnail URL</td>
+						<td>{channelThumbnailUrl}</td>
+					</tr>
+				</tbody>
 			</table>
 
 			<style jsx>{style}</style>
