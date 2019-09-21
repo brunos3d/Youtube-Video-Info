3f174a9c050b8f6af0248ea7b7500fa2f688b3cc
Bruno Silva <bruno3dcontato@gmail.com>
2019-09-21 01:32:49 -0300

    added hello world page for some tests

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ const fetchVideoInfo = require("youtube-info");
 app.prepare().then(() => {
 	const server = express();
 
+	server.get("/", (req, res) => {
+		return app.render(req, res, "/", req.query);
+	});
+
+	server.get("/hello", (req, res) => {
+		return app.render(req, res, "/hello", req.query);
+	});
+
 	server.get("/video/:id", async (req, res) => {
 		const data = await fetchVideoInfo(req.params.id);
 		console.log("pass 1", data);
 		return app.render(req, res, "/video/:id", { data });
 	});
-
-	server.get("/", (req, res) => {
-		return app.render(req, res, "/", req.query);
-	});
-
-	// server.get("/b", (req, res) => {
-	// 	return app.render(req, res, "/b", req.query);
-	// });
 
 	// server.get("/posts/:id", (req, res) => {
 	// 	return app.render(req, res, "/posts", { id: req.params.id });
